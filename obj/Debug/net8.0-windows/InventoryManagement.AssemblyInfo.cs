d3577a609dd833830c008e137b0e14f7ecd57403
BachLaToi <Bachdeptrai04@gmail.com>
2025-10-28 11:21:09 +0700

    update task

--- obj/Debug/net8.0-windows/InventoryManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/InventoryManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d612869c267363b3e2064e45c38d589b01cf2c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
